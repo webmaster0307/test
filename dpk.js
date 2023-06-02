47ab435017b69c4fad9803cfcb46cfa76ef67326
Web Master <web.master0307@gmail.com>
2023-06-02 07:39:39 -0400

    simplified conditional statements

--- dpk.js
+++ dpk.js
@@ -3,26 +3,26 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let partitionKey;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  if (event && event.partitionKey) {
+    partitionKey = event.partitionKey;
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    partitionKey = crypto.createHash("sha3-512").update(data).digest("hex");
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+
+  if (!partitionKey) {
+    partitionKey = TRIVIAL_PARTITION_KEY;
   }
-  return candidate;
+
+  if (typeof partitionKey !== "string") {
+    partitionKey = JSON.stringify(partitionKey);
+  }
+
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
+  }
+
+  return partitionKey;
 };
